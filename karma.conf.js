6e1a524f038338cf56e599d543b11ca7e6ab7a00
unknown <juan.quintero@glb-d2759.globant.com>
2015-07-02 17:27:15 -0300

    working code running typescript with jasmine on karma

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,8 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     // https://github.com/karma-runner/karma/issues/481
     files: [
-      'src/**/*.js',
-      'spec/**/*.spec.js',
+      'src/**/*.ts',
+      'spec/**/*.spec.ts',
       // fixtures
       {
         pattern: 'spec/**/*.json',
@@ -36,6 +36,27 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.ts': ['typescript']
+    },
+
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler 
+      options: {
+        sourceMap: false, // (optional) Generates corresponding .map file. 
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5' 
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd' 
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type. 
+        noResolve: true, // (optional) Skip resolution and preprocessing. 
+        removeComments: true // (optional) Do not emit comments to output. 
+      },
+      // extra typing definitions to pass to the compiler (globs allowed) 
+      // typings: [
+      //   'typings/tsd.d.ts'
+      // ],
+      // transforming the filenames 
+      transformPath: function (path) {
+        return path.replace(/\.ts$/, '.js');
+      }
     },
 
 
